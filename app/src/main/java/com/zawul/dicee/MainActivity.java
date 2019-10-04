d166225bb97f8575cbecbae386a7dd5ebf8f0ad4
suhrab <suhrabahdi@gmail.com>
2019-10-04 12:48:46 -0700

    Completed project

---
+++ app/src/main/java/com/zawul/dicee/MainActivity.java
@@ -0,0 +1,45 @@
+package com.zawul.dicee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imgleft;
+    ImageView imgright;
+    Button btnroll;
+int[] imgDices =  {
+        R.drawable.dice1,
+        R.drawable.dice2,
+        R.drawable.dice3,
+        R.drawable.dice4,
+        R.drawable.dice5,
+        R.drawable.dice6,
+};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imgleft = findViewById(R.id.imgleft);
+        imgright = findViewById(R.id.imgright);
+        btnroll = findViewById(R.id.btnroll);
+        btnroll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random rRandom = new Random();
+                int number = rRandom.nextInt(6);
+               imgleft.setImageResource(imgDices[number]);
+               number = rRandom.nextInt(6);
+               imgright.setImageResource(imgDices[number]);
+            }
+        });
+    }
+}
